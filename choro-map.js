e2312cb3b8246cf5771f8a5e7e4ccaa33a36c1e8
Reon-Richards <rr7366@gmail.com>
2023-07-08 11:09:20 +0900

    refactor, remove FCC tester

--- choro-map.js
+++ choro-map.js
@@ -15,7 +15,6 @@ const countyResponse = await fetch(countyDataURL)
 createPaths();
 }
 
-
 foo();
 
 d3.select("#mapBoard")
@@ -37,66 +36,59 @@ let createPaths = ()=>{
     let county = eduData.find((item) => {
     return item.fips === id
     })
-  let percent = county['bachelorsOrHigher'];
-        if(percent <= 10) {
-          return '#f7fbff'
-        }else if(percent <= 20) {
-          return '#deebf7'          
-        }else if(percent <= 30) {
-          return '#c6dbef'
-        }else if(percent <= 40) {
-          return '#9ecae1'
-        }else if(percent <= 50) {
-          return '#6baed6'
-        }else if(percent <= 60) {
-          return '#4292c6'
-        }else if(percent <= 70) {
-          return '#2171b5'
-        }else if(percent <= 80) {
-          return '#08519c'
-        }else if(percent <= 90) {
-          return '#08306b'
-        }else if(percent <= 100) {
-          return  '#041f47'
-        }
+    let percent = county['bachelorsOrHigher'];
+      if(percent <= 10) {
+        return '#f7fbff'
+      }else if(percent <= 20) {
+        return '#deebf7'          
+      }else if(percent <= 30) {
+        return '#c6dbef'
+      }else if(percent <= 40) {
+        return '#9ecae1'
+      }else if(percent <= 50) {
+        return '#6baed6'
+      }else if(percent <= 60) {
+        return '#4292c6'
+      }else if(percent <= 70) {
+        return '#2171b5'
+      }else if(percent <= 80) {
+        return '#08519c'
+      }else if(percent <= 90) {
+        return '#08306b'
+      }else if(percent <= 100) {
+        return  '#041f47'
+      }
 })
 .attr('data-fips', (d)=>{
   return d.id
 })
 .attr('data-education', (d)=>{
   let id = d['id']
-          let county = eduData.find((d) => {
-            return d.fips === id
-               })
-               let percent = county['bachelorsOrHigher'];
-               return percent
+  let county = eduData.find((d) => {
+    return d.fips === id
+    })
+  let percent = county['bachelorsOrHigher'];
+    return percent
 })
-        .on('mouseover', (event,item) => {
-          tooltip.style('visibility', 'visible')
-                .style('left', event.pageX + 20 + 'px')
-                .style('top', event.pageY - 28 + 'px')
-          
-                let id = item['id']
-                let county = eduData.find((item) => {
-                  return item.fips === id
-                     })
-          
-          tooltip.attr('data-education', county['bachelorsOrHigher'])
-          
-          tooltip.html(county.area_name + ', ' + county.state +' : ' + county.bachelorsOrHigher + '%')  
-                     
-              })
+  .on('mouseover', (event,item) => {
+    tooltip.style('visibility', 'visible')
+          .style('left', event.pageX + 20 + 'px')
+          .style('top', event.pageY - 28 + 'px')
+    
+    let id = item['id']
+    let county = eduData.find((item) => {
+      return item.fips === id
+      })
+    
+    tooltip.attr('data-education', county['bachelorsOrHigher'])
+    
+    tooltip.html(county.area_name + ', ' + county.state +' : ' + county.bachelorsOrHigher + '%')  
+  })
 
-        .on('mouseout', (event, item) => {
-          tooltip.style('visibility', 'hidden')
-        })
-
-
-        console.log("edudata ",  eduData)
-        console.log("county ",  countyData.objects)
-
-
-        }
+  .on('mouseout', (event, item) => {
+    tooltip.style('visibility', 'hidden')
+  })
+}
 
 
 
